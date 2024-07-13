228e640d4dba7444c4600dce8fb0f338400d8e96
Shivansh Singh <shivanshsingh9670@gmail.com>
2024-07-13 08:02:24 +0530

    REST API | GET functionality

---
+++ bootrestbook/src/main/java/com/api/book/bootrestbook/controllers/BookController.java
@@ -0,0 +1,27 @@
+package com.api.book.bootrestbook.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.book.bootrestbook.entities.Book;
+import com.api.book.bootrestbook.services.BookService;
+
+@RestController
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/books")
+    public List<Book> GetBooks() {
+        return this.bookService.GetAllBooks();
+    }
+
+    @GetMapping("/books/{id}")
+    public Book GetBook(@PathVariable("id") int id) {
+        return bookService.GetBookById(id);
+    }
+}
---
+++ bootrestbook/src/main/java/com/api/book/bootrestbook/services/BookService.java
@@ -0,0 +1,30 @@
+package com.api.book.bootrestbook.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.api.book.bootrestbook.entities.Book;
+
+@Component
+public class BookService {
+    static List<Book> list = new ArrayList<>();
+
+    static {
+        list.add(new Book(2, "Complete C++", "William"));
+        list.add(new Book(3, "Complete Python", "Nathasha"));
+    }
+
+    // get all books
+    public List<Book> GetAllBooks() {
+        return list;
+    }
+
+    // get single book by id
+    public Book GetBookById(int id) {
+        Book book = null;
+        book = list.stream().filter(e -> e.getId() == id).findFirst().get();
+        return book;
+    }
+}
