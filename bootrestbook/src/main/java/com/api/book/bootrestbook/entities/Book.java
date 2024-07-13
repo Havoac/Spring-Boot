d7efaca83ed354089c4a6864f51e812e6a9b25e6
Shivansh Singh <shivanshsingh9670@gmail.com>
2024-07-13 21:43:50 +0530

    One to One mapping

--- bootrestbook/src/main/java/com/api/book/bootrestbook/entities/Book.java
+++ bootrestbook/src/main/java/com/api/book/bootrestbook/entities/Book.java
@@ -1,10 +1,12 @@
 package com.api.book.bootrestbook.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,13 +16,21 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "book_id")
     private int id;
+
     private String title;
-    private String author;
+
+    // we use CascadeType b/c all operations (PERSIST, MERGE, REMOVE, REFRESH,
+    // DETACH) performed on the parent entity are automatically cascaded to the
+    // associated entity. This means that if you persist, merge, remove, refresh, or
+    // detach the parent entity, the same operation will be automatically applied to
+    // the associated entity.
+    @OneToOne(cascade = CascadeType.ALL)
+    private Author author;
 
     public Book() {
     }
 
-    public Book(int id, String title, String author) {
+    public Book(int id, String title, Author author) {
         this.id = id;
         this.title = title;
         this.author = author;
@@ -42,11 +52,11 @@ public class Book {
         this.title = title;
     }
 
-    public String getAuthor() {
+    public Author getAuthor() {
         return author;
     }
 
-    public void setAuthor(String author) {
+    public void setAuthor(Author author) {
         this.author = author;
     }
 
