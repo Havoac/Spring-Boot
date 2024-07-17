6edc1dd743882a7647433d5639a2952d58bbed4d
Shivansh Singh <shivanshsingh9670@gmail.com>
2024-07-17 08:02:54 +0530

    Iteration | Conditional Statement in Thymeleaf

--- thymeleafPractical/src/main/java/com/practice/controller/MyController.java
+++ thymeleafPractical/src/main/java/com/practice/controller/MyController.java
@@ -1,9 +1,11 @@
 package com.practice.controller;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -17,5 +19,25 @@ public class MyController {
 		model.addAttribute("currentDate", new Date().toLocaleString());
 
 		return "about"; // about.html
+	}
+
+	// handling iteration
+	@RequestMapping("/example-loop")
+	public String IterateHandler(Model m) {
+		// create a list, set collection
+		List<String> names = List.of("Aman", "Laxmi", "Jack");
+		m.addAttribute("ns", names);
+		return "iterate";
+	}
+
+	// handling conditional statements
+	@GetMapping("/condition")
+	public String ConditionalHandler(Model m) {
+		m.addAttribute("gender", "M");
+
+		List<Integer> list = List.of(1,2,3,4);
+		m.addAttribute("list", list);
+
+		return "condition";
 	}
 }
